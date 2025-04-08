64b47a381e94b06edc4aaa1a7c0295f5a28e79f7
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2025-04-09 03:30:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def download_audio_from_youtube(youtube_url, download_path):
     try:
         if not os.path.exists(download_path):
             os.makedirs(download_path)
-        command = f'yt-dlp -o "{download_path}/%(title)s.%(ext)s" -x --audio-format mp3 "{youtube_url}"'
         command = f'yt-dlp -4 -o "{download_path}/%(title)s.%(ext)s" -x --audio-format mp3 "{youtube_url}"'
         result = subprocess.run(command, shell=True)
         if result.returncode == 0:
@@ -49,25 +48,25 @@ st.title("YouTube Audio Separator")
 st.write("Enter a YouTube URL to download, separate, and play the audio stems.")
 
 # Input field from app
-youtube_url = st.text_input("YouTube URL", "https://music.youtube.com/watch?v=wxHRqPtWHOU")
+youtube_url = st.text_input("YouTube URL", "https://music.youtube.com/watch?v=BNkIvh6qExw")
 download_path = "content"
 output_path = "output"
 
 if st.button("Process Audio"):
     downloaded_file = download_audio_from_youtube(youtube_url, download_path)
-
-if downloaded_file:
-    separate_audio(downloaded_file, output_path)
-
-    song_name = os.path.splitext(os.path.basename(downloaded_file))[0]
-    vocals_wav = os.path.join(output_path, song_name, "vocals.wav")
-    vocals_mp3 = os.path.join(output_path, song_name, "vocals.mp3")
-
-    if os.path.exists(vocals_wav):
-        mp3_file = convert_wav_to_mp3(vocals_wav, vocals_mp3)
-        if mp3_file and os.path.exists(mp3_file):
-            st.write("### Vocals Audio")
-            audio_file = open(mp3_file, "rb")
-            st.audio(audio_file.read(), format="audio/mp3")
-    else:
-        st.error("Vocals WAV file not found!")
+    
+    if downloaded_file:
+        separate_audio(downloaded_file, output_path)
+        
+        song_name = os.path.splitext(os.path.basename(downloaded_file))[0]
+        vocals_wav = os.path.join(output_path, song_name, "vocals.wav")
+        vocals_mp3 = os.path.join(output_path, song_name, "vocals.mp3")
+        
+        if os.path.exists(vocals_wav):
+            mp3_file = convert_wav_to_mp3(vocals_wav, vocals_mp3)
+            if mp3_file and os.path.exists(mp3_file):
+                st.write("### Vocals Audio")
+                audio_file = open(mp3_file, "rb")
+                st.audio(audio_file.read(), format="audio/mp3")
+        else:
+            st.error("Vocals WAV file not found!")
